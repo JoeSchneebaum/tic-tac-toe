6088a75efb0fcee471e259247afea5902284a55f
JoeSchneebaum <61995731+JoeSchneebaum@users.noreply.github.com>
2020-03-17 16:19:33 -0400

    Added location (col, row) to history list.

--- src/index.js
+++ src/index.js
@@ -65,7 +65,8 @@ class Game extends React.Component {
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       history: history.concat([{
-        squares: squares
+        squares: squares,
+        latestMoveSquare: i
       }]),
       stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
@@ -82,12 +83,16 @@ class Game extends React.Component {
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
+    const stepNumber = this.state.stepNumber;
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
-      const desc = move ?
-        'Go to move #' + move :
-        'Go to game start';
+    const latestMoveSquare = step.latestMoveSquare;
+    const col = 1 + latestMoveSquare % 3;
+    const row = 1 + Math.floor(latestMoveSquare / 3);
+    const desc = move ?
+      `Go to move #${move} (${col}, ${row})` :
+      'Go to game start';
       return (
         <li key={move}>
           <button onClick={() => this.jumpTo(move)}>{desc}</button>
